c0e12618a447905217fce084834ff88f962038c7
codenoobies <kevindubidaam@gmail.com>
2024-01-18 15:50:20 +0700

    feat: page Information for User

--- src/Components/Base/MyPagination.js
+++ src/Components/Base/MyPagination.js
@@ -9,7 +9,8 @@ function MyPagination(props) {
     }, [props.currentPage])
 
     return (
-        <div className="flex justify-end gap-3">
+        <div className="flex justify-end gap-3 items-center">
+            <p className="m-0">{props.currentPage} of {props.maxPage}</p>
             <input type="number" className="border-solid border-2 w-full sm:w-20 border-gray-500 rounded-sm px-2 focus:outline-none" min="1" max="200" value={props.currentPage} onChange={props.pageInputChange} />
             <div className="flex gap-2">
                 <Button text={<CaretLeftFill />} callback={() => props.handlePageChange(-1)} disabled={props.currentPage <= 1} />
